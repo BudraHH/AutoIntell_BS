955eb0ddb29e3be18979f47ac81bc90ef783c3af
budrahh <hariharabudra@gmail.com>
2025-03-31 20:37:33 +0530

    del

--- authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import login_user, register_user
-
-urlpatterns = [
-    path('sign-in/', login_user, name='sign-in'),
-    path('sign-up/', register_user, name='sign-up'),
-]
--- sensor_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import VehicleSensorData
-
-class VehicleSensorDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleSensorData
-        fields = '__all__'
